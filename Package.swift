265bba748217afceabd82306ed3ded0e4bf3a092
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-08-02 04:18:12 +0800

    Add dependencies for Syntax target to include LogoEngine

--- Package.swift
+++ Package.swift
@@ -27,7 +27,8 @@ let package = Package(
             name: "TextTransform"
         ),
         .target(
-            name: "Syntax"
+            name: "Syntax",
+            dependencies: ["LogoEngine"]
         ),
         .target(
             name: "Diagram"
