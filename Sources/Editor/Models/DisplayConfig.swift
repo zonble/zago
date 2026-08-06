1a46dd64a285c3d67d7330b652a10c2349b9e54f
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-08-07 01:48:05 +0800

    refactor(editor): move DisplayConfig struct to Models directory as a standalone model

---
+++ Sources/Editor/Models/DisplayConfig.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+/// Value object storing display preferences and UI toggle options for the editor.
+public struct DisplayConfig: Sendable, Equatable {
+    /// Whether the WordStar ruler bar is enabled.
+    public var showRuler: Bool
+
+    /// Whether line numbers gutter is enabled.
+    public var showLineNumbers: Bool
+
+    /// Whether sub-line wrapping numbers are enabled.
+    public var showSubLineNumbers: Bool
+
+    /// Whether ANSI syntax highlighting is enabled.
+    public var enableSyntaxHighlight: Bool
+
+    /// Whether automatic file reload on external modification is enabled.
+    public var autoReload: Bool
+
+    /// Number of spaces representing a Tab character.
+    public var tabSize: Int
+
+    /// Whether trailing whitespace is automatically trimmed on save.
+    public var trimTrailingWhitespaceOnSave: Bool
+
+    /// Whether Git diff indicators in gutter are enabled.
+    public var showGitDiff: Bool
+
+    public init(
+        showRuler: Bool = false,
+        showLineNumbers: Bool = true,
+        showSubLineNumbers: Bool = false,
+        enableSyntaxHighlight: Bool = true,
+        autoReload: Bool = true,
+        tabSize: Int = 4,
+        trimTrailingWhitespaceOnSave: Bool = false,
+        showGitDiff: Bool = true
+    ) {
+        self.showRuler = showRuler
+        self.showLineNumbers = showLineNumbers
+        self.showSubLineNumbers = showSubLineNumbers
+        self.enableSyntaxHighlight = enableSyntaxHighlight
+        self.autoReload = autoReload
+        self.tabSize = tabSize
+        self.trimTrailingWhitespaceOnSave = trimTrailingWhitespaceOnSave
+        self.showGitDiff = showGitDiff
+    }
+}
