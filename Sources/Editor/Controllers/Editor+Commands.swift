ffcbd62f4bb0ed08e5dc3862af2e115be7eca0d7
zonble <zonble@gmail.com>
2026-08-09 19:30:37 +0800

    refactor(editor): move diagram command out of LOGO primitives into dedicated DiagramMenuCommand in Editor Commands

--- Sources/Editor/Controllers/Editor+Commands.swift
+++ Sources/Editor/Controllers/Editor+Commands.swift
@@ -93,21 +93,6 @@ extension Editor {
         commandRegistry.register(ToggleCanvasModeCommand())
         commandRegistry.register(ToggleTableModeCommand())
         commandRegistry.register(CycleBorderStyleCommand())
-        commandRegistry.register(
-            BlockCommand(
-                id: .diagramMenu,
-                name: "diagram.menu",
-                description: "Open diagram snippet menu",
-                keys: [],
-                commandBarAliases: ["diagram", "diagrams", "snippet", "snippets"]
-            ) { editor in
-                editor.menuBar.updateCategories(for: editor)
-                if let idx = editor.menuBar.categories.firstIndex(where: { $0.titleKey == "menu.diagrams" }) {
-                    editor.menuBar.categoryIndex = idx
-                    editor.menuBar.itemIndex = 0
-                }
-                editor.isMenuBarActive = true
-            }
-        )
+        commandRegistry.register(DiagramMenuCommand())
     }
 }
