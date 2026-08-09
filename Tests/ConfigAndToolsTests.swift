46013bf586e394003de0f36da838a583c6e6ea61
zonble <zonble@gmail.com>
2026-08-09 14:57:07 +0800

    Release: Version 1.2.4

--- Tests/ConfigAndToolsTests.swift
+++ Tests/ConfigAndToolsTests.swift
@@ -17,7 +17,7 @@ struct ConfigAndToolsTests {
     }
     @Test func testZagoVersionAndTitleBarDisplay() throws {
     #expect(!ZagoVersion.current.isEmpty)
-    #expect(ZagoVersion.current == "1.2.3")
+    #expect(ZagoVersion.current == "1.2.4")
 
     let editor = Editor()
     let titleLine = editor.renderer.renderTitleOrMenuBar(editor: editor, cols: 80)
