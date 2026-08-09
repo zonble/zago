00547a402790d075f8d11af74414bcc2620eb75c
zonble <zonble@gmail.com>
2026-08-09 16:20:32 +0800

    Bump version to 1.2.5

--- Tests/ConfigAndToolsTests.swift
+++ Tests/ConfigAndToolsTests.swift
@@ -17,7 +17,7 @@ struct ConfigAndToolsTests {
     }
     @Test func testZagoVersionAndTitleBarDisplay() throws {
     #expect(!ZagoVersion.current.isEmpty)
-    #expect(ZagoVersion.current == "1.2.4")
+    #expect(ZagoVersion.current == "1.2.5")
 
     let editor = Editor()
     let titleLine = editor.renderer.renderTitleOrMenuBar(editor: editor, cols: 80)
