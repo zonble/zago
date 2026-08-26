b9ad2a28b8a5464cff50048d4ed9f6e9b55b711c
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-08-26 21:12:01 +0800

    docs(config): add sample transliteration procedures to zagorc template

--- Sources/Config/ConfigLoader.swift
+++ Sources/Config/ConfigLoader.swift
@@ -489,6 +489,19 @@ public final class ConfigLoader {
             #     MOVE LEFT (:boxWidth - 1) MOVE UP 2
             #     FILL :text
             #   END
+            #
+            #   ; TO TOHANGUL :str op TRANSLIT "Any-Hangul" :str END
+            #   ; TO TOTHAI :str op TRANSLIT "Any-Thai" :str END
+            #   ; TO TOGREEK :str op TRANSLIT "Any-Greek" :str END
+            #   ; TO TOCYRILLIC :str op TRANSLIT "Any-Cyrillic" :str END
+            #   ; TO TOARABIC :str op TRANSLIT "Any-Arabic" :str END
+            #   ; TO TOHEBREW :str op TRANSLIT "Any-Hebrew" :str END
+            #   ; TO TODEVANNAGARI :str op TRANSLIT "Any-Devanagari" :str END
+            #   ; TO TOTAMIL :str op TRANSLIT "Any-Tamil" :str END
+            #   ; TO TOTELUGU :str op TRANSLIT "Any-Telugu" :str END
+            #   ; TO TOBENGALI :str op TRANSLIT "Any-Bengali" :str END
+            #   ; TO TOGEOGIAN :str op TRANSLIT "Any-Georgian" :str END
+            #
             # endlogo
             #
             # logo-script insert-title
