9ae7f542510b518bf344ea81ce99ca7eea2bdbd5
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-08-27 01:28:00 +0800

    chore: sync homebrew template sha256

--- packaging/homebrew/zago.rb
+++ packaging/homebrew/zago.rb
@@ -2,7 +2,7 @@ class Zago < Formula
   desc "Terminal text editor and plain-text diagramming tool"
   homepage "https://github.com/zonble/zago"
   url "https://github.com/zonble/zago/archive/refs/tags/v1.4.2.tar.gz"
-  sha256 "1a74f789bba7b77603bd590359fbae87ec7781abeadce4022b3258cdbc299a81"
+  sha256 "065fc1945b023583f2f897f370a2b33e4f4eb9313f1e6969e95f8d3f1c36ca24"
   license "MIT"
   head "https://github.com/zonble/zago.git", branch: "main"
 
