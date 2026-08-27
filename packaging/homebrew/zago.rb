888fc212b70257698d71cef40fcf2918604e4c8d
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-08-28 03:53:40 +0800

    chore(homebrew): update sha256 for v1.4.4

--- packaging/homebrew/zago.rb
+++ packaging/homebrew/zago.rb
@@ -2,7 +2,7 @@ class Zago < Formula
   desc "Terminal text editor and plain-text diagramming tool"
   homepage "https://github.com/zonble/zago"
   url "https://github.com/zonble/zago/archive/refs/tags/v1.4.4.tar.gz"
-  sha256 "065fc1945b023583f2f897f370a2b33e4f4eb9313f1e6969e95f8d3f1c36ca24"
+  sha256 "dc5a2c64e6a025ff339f6f77f2b212c2fe6f06c1e5863f0ed1f869aa94cc6cc5"
   license "MIT"
   head "https://github.com/zonble/zago.git", branch: "main"
 
